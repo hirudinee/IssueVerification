6a130578567c7caf37ade1d19ee823283fb5ce16
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-12 15:11:57 +0530

    redeply

--- IssueVerification/lambda.js
+++ IssueVerification/lambda.js
@@ -1,8 +1,9 @@
 let AWS = require('aws-sdk');
-const sqs = new SL.AWS.SQS(AWS);
+
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
+const sqs = new SL.AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
 
 	// You must always end/destroy the DB connection after it's used
@@ -15,15 +16,20 @@ exports.handler = function (event, context, callback) {
 
 	// callback(null, 'Successfully executed');
 	sqs.sendMessage({
-		MessageBody: 'this is a test',
+		MessageBody: 'this is a test edit',
 		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/263248768798/sample',
 		DelaySeconds: '0',
-		MessageAttributes: {}
+		MessageAttributes: {
+			"sample": {
+				"DataType": "String",
+				"StringValue": "test"
+			}
+		}
 	}, function (data) {
-		console.log('successfull',data);
+		console.log('successfull', data);
 	}, function (error) {
-		console.log('error ',error);
-		
+		console.log('error ', error);
+
 	});
 
 
---
+++ IssueVerification/sample.js
@@ -0,0 +1,32 @@
+let AWS = require('aws-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+let connectionManager = require('./ConnectionManager');
+let SL = require('@slappforge/slappforge-sdk');
+const rds = new SL.AWS.RDS(connectionManager);
+exports.handler = function (event, context, callback) {
+
+	// You must always end/destroy the DB connection after it's used
+	rds.beginTransaction({
+		instanceIdentifier: 'DBid'
+	}, function (error, connection) {
+		if (error) { throw err; }
+	});
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/263248768798/sample',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0',
+		MessageAttributeNames: ['']
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
+		})
+	}, function (error) {
+		// implement error handling logic here
+	});
+
+
+	callback(null, 'Successfully executed');
+}
