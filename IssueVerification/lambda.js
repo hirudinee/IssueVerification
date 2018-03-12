abaa8a1df852f7f2b0fa720e56a49761ad4f7de4
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-12 17:46:52 +0530

    sqs receive message

--- IssueVerification/lambda.js
+++ IssueVerification/lambda.js
@@ -42,5 +42,24 @@ exports.handler = function (event, context, callback) {
 
 	});
 
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/263248768798/sample',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0',
+		MessageAttributeNames: ['']
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
+			console.log(message);
+		})
+	}, function (error) {
+		// implement error handling logic here
+		console.log(error);
+	});
+
+
 
 }
