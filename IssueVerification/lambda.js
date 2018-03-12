407d9626e20b268edb2a783b0ff3ad48da3fda5f
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-12 17:18:07 +0530

    adding RDS

--- IssueVerification/lambda.js
+++ IssueVerification/lambda.js
@@ -6,12 +6,12 @@ const rds = new SL.AWS.RDS(connectionManager);
 
 exports.handler = function (event, context, callback) {
 
-	// You must always end/destroy the DB connection after it's used
-	// rds.beginTransaction({
-	// 	instanceIdentifier: 'DBid'
-	// }, function (error, connection) {
-	// 	if (error) { throw err; }
-	// });
+	//You must always end/destroy the DB connection after it's used
+	rds.beginTransaction({
+		instanceIdentifier: 'DBid'
+	}, function (error, connection) {
+		if (error) { throw err; }
+	});
 
 
 	// callback(null, 'Successfully executed');
